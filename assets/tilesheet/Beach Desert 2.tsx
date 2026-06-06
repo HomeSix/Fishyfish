c997cd925abaae81f5a1de925f8567677064d7a0
azik <afifzikri122@gmail.com>
2026-06-07 00:02:01 +0800

    added proper tiled integration

---
+++ assets/tilesheet/Beach Desert 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Beach Desert 2" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../spreadsheet/beach-desert-2.png" width="1024" height="1024"/>
+</tileset>
