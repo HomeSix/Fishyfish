6aa37f1bdd80afec1d37c5f7020d3ae72e291f8c
azik <afifzikri122@gmail.com>
2026-06-07 01:16:29 +0800

    added museum map

---
+++ assets/tilesheet/anas/rocks-snow-overlay.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="rocks-snow-overlay" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../spreadsheet/anas/rocks-snow-overlay.png" width="1024" height="1024"/>
+</tileset>
---
+++ assets/tilesheet/basic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="basic" tilewidth="32" tileheight="32" tilecount="448" columns="16">
+ <image source="../spreadsheet/basic.png" width="512" height="896"/>
+</tileset>
