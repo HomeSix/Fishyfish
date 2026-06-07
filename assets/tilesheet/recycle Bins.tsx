e60696a818a3ec767d28f2a3311f1f4fe31a11b2
azik <afifzikri122@gmail.com>
2026-06-07 10:01:10 +0800

    added completed beach map

---
+++ assets/tilesheet/recycle Bins.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="recycle Bins" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="../spreadsheet/recycleBins.png" width="96" height="32"/>
+</tileset>
